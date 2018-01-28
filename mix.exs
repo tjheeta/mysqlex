db654d26a917b3eb0e02b34eede74ce04b2cd3a6
Tejinder Jheeta <tejinder.jheeta@autodesk.com>
2018-01-27 17:29:58 -0800

    Update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mysqlex.Mixfile do
 
   def project() do
     [app: :mysqlex,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      description: description(),
      package: package(),
